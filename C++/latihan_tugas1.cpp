ab3a738aa9da57e16331638a81e8223e863e96cb
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2022-09-28 09:45:16 +0700

    update

---
+++ C++/latihan_tugas1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char nama[20],kelas, alamat;
+    int nim, angkatan;
+    printf("\tTugas 1 Praktikum Dasar Dasar Pemrograman\n");
+   
+    printf("Masukan nama anda : "); scanf("%s", &nama);
+    printf("hello %s", nama);
+}
+
+// var hasil = "";
+// for (var i = 1; i == 1; i++) {
+//     for (var j = 0; j < 40; j++) {
+//         hasil += '-';
+//            }   
+//            hasil += '\n';
+// }
+// console.log(hasil);
