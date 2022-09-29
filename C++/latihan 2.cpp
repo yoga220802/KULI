f88e2bb3c353b6b9203e582092171c088bb43826
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2022-09-29 17:33:14 +0700

    update

---
+++ C++/latihan 2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+//	int test = 25;
+	sqrt(25);
+}
--- Python/tugas1.py
+++ Python/tugas1.py
@@ -5,12 +5,7 @@ nim = input("Masukan nama NIM \t\t: ")
 kelas = input("Masukan kelas anda \t\t: ")
 angkatan = input("Masukan tahun angkatan anda \t: ")
 alamat = input("Masukan alamat anda \t\t: ")
-hidup = input("Apakah anda masih hidup ? (Y/n)")
-
-if hidup == "Y" or hidup == "y" : greeting = "Semoga hari anda menyenangkan"
-else: greeting = "semoga anda tenang di alam sana"
-
-# data = ("Hallo {}".format(nama))
 print("\t"+"_"*41)
-dataDiri = (f"Hallo {nama} dengan NIM {nim} dari kelas {kelas} angkatan tahun {angkatan}.\nAlamat anda berada di {alamat}. {greeting}")
-print(dataDiri)
+data = ("Hallo {} dengan NIM {} dari kelas {} angkatan tahun {}. Alamat anda berada di {}".format(nama, nim, kelas, angkatan, alamat))
+# dataDiri = (f"Hallo {nama} dengan NIM {nim} dari kelas {kelas} angkatan tahun {angkatan}. Alamat anda berada di {alamat}.")
+print(data)
