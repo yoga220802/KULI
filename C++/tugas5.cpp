e7250072fa460c9e3b600e8098379da5649a72ab
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2022-11-04 20:22:19 +0700

    apdet

--- C++/tugas5.cpp
+++ C++/tugas5.cpp
@@ -1,44 +1,73 @@
-#include <stdio.h>
-#include <string>
+#include <iostream>
+using namespace std;
 int main(){
     // deklarasi variabel
-    float tugas1, tugas2, tugas3, tugas4, uts, uas, total, rataTugas;
-    char grade;
-    std::string nama, nim;
+    float tugas1, tugas2, tugas3, tugas4, uts, uas, total;
+    string grade, status;
     // input untuk value variabel
-    printf("Masukan nama anda : "); scanf("%[^\n]s", &nama);
-    printf("Masukan NIM anda : "); scanf("%d", &nim);
-    printf("Masukan nilai tugas 1 : "); scanf("%f", &tugas1);
-    printf("Masukan nilai tugas 2 : "); scanf("%f", &tugas2);
-    printf("Masukan nilai tugas 3 : "); scanf("%f", &tugas3);
-    printf("Masukan nilai tugas 4 : "); scanf("%f", &tugas4);
-    printf("Masukan nilai UTS : "); scanf("%f", &uts);
-    printf("Masukan nilai UAS : "); scanf("%f", &uas);
-    // assignment untuk value variabel
-    rataTugas = (tugas1+tugas2+tugas3+tugas4)/4;
-    total = (rataTugas*0.4) + (uas*0.3) + (uts*0.3);
+    cout << ("Masukan nilai tugas 1 : "); cin >> (tugas1);
+    cout << ("Masukan nilai tugas 2 : "); cin >> (tugas2);
+    cout << ("Masukan nilai tugas 3 : "); cin >> (tugas3);
+    cout << ("Masukan nilai tugas 4 : "); cin >> (tugas4);
+    cout << ("Masukan nilai UTS : "); cin >> (uts);
+    cout << ("Masukan nilai UAS : "); cin >> (uas);
+    // assignment untuk menghitung rata-rata tugas dan total nilai
+    total = (((tugas1+tugas2+tugas3+tugas4)/4)*0.4) + (uas*0.3) + (uts*0.3);
     // percabangan untuk menetukan grade
     if (total > 80){
-        printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, lulus dengan predikat A",nama, nim, total);
-        // grade = 'A';
+        grade = "A";
+        status = "lulus";
     }else if (total > 70){
-        printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, lulus dengan predikat B",nama, nim, total);
-    //    grade = 'B';
+       grade = "B";
+        status = "lulus";
     }else if (total > 60){
-        printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, lulus dengan predikat C",nama, nim, total);
-    //    grade = 'C';
+       grade = "C";
+        status = "lulus";
     }else{
-        printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, tidak lulus dengan predikat D",nama, nim, total);
-    //    grade = 'D';
+       grade = "D";
+        status = "tidak lulus";
     }
-    // percabangan untuk menampilkan teks kelulusan
-    // switch (grade)
-    // {
-    //     case 'D':
-    //         printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, tidak lulus dengan predikat %c",nama, nim, total, grade);
-    //         break;
-    //     default:
-    //         printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %.2f, lulus dengan predikat %c",nama, nim, total, grade);
-    //         break;
-    // }
+    // menampilkan hasil kalkulasi dan grading
+    cout << "Mahasiswa mendapat nilai "<<total<<" , "<<status<<" dengan predikat "<<grade;
 }
+// #include <stdio.h>
+// #include <string>
+// using namespace std;
+// int main(){
+//     // deklarasi variabel
+//     float tugas1, tugas2, tugas3, tugas4, uts, uas, total, rataTugas;
+//     char grade;
+//     string nama, nim;
+//     // input untuk value variabel
+//     printf("Masukan nama anda : "); scanf("%[^\n]s", &nama);
+//     printf("Masukan NIM anda : "); scanf("%d", &nim);
+//     printf("Masukan nilai tugas 1 : "); scanf("%f", &tugas1);
+//     printf("Masukan nilai tugas 2 : "); scanf("%f", &tugas2);
+//     printf("Masukan nilai tugas 3 : "); scanf("%f", &tugas3);
+//     printf("Masukan nilai tugas 4 : "); scanf("%f", &tugas4);
+//     printf("Masukan nilai UTS : "); scanf("%f", &uts);
+//     printf("Masukan nilai UAS : "); scanf("%f", &uas);
+//     // assignment untuk menghitung rata-rata tugas dan total nilai
+//     rataTugas = (tugas1+tugas2+tugas3+tugas4)/4;
+//     total = (rataTugas*0.4) + (uas*0.3) + (uts*0.3);
+//     // percabangan untuk menetukan grade
+//     if (total > 80){
+//         grade = 'A';
+//     }else if (total > 70){
+//        grade = 'B';
+//     }else if (total > 60){
+//        grade = 'C';
+//     }else{
+//        grade = 'D';
+//     }
+//     // percabangan untuk menampilkan teks kelulusan
+//     switch (grade)
+//     {
+//         case 'D':
+//             printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %f, tidak lulus dengan predikat %c",nama, nim, total, grade);
+//             break;
+//         default:
+//             printf("Mahasiswa dengan nama %s dan NIM %d mendapat nilai %.2f, lulus dengan predikat %c",nama, nim, total, grade);
+//             break;
+//     }
+// }
---
+++ C++/tugas6_ngide.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string makanan[] = {"Kue", "Gorengan", "Bakso"}, minuman[] = {"Teh", "Kopi", "Air Mineral"}, namaProduk;
+    int hargaMakanan[] = {5000, 2000, 7000}, hargaMinuman[] = {4000, 5000, 3000}, hargaProduk;
+    int jml_arr = sizeof(makanan)/sizeof(*makanan), jenis, produk, jumlah;
+    cout<<"\t---Jenis Produk---\n1. Makanan\n2. Minuman\nPilih jenis produk : ";
+    cin>>(jenis);
+    switch (jenis)
+    {
+    case 1:
+        cout<<"\n\t--Daftar Makanan--"<<endl;
+        for (int i = 0; i < jml_arr; i++)
+            {
+                cout<<i+1<<". "<<makanan[i]<<" : "<<hargaMakanan[i]<<endl;
+            }
+        cout<<"Pilih makanan : ";
+        cin>>(produk);
+        switch (produk)
+        {
+        case 1:
+            namaProduk = makanan[0];
+            hargaProduk = hargaMakanan[0];
+            break;
+        case 2:
+            namaProduk = makanan[1];
+            hargaProduk = hargaMakanan[1];
+            break;
+        case 3:
+            namaProduk = makanan[2];
+            hargaProduk = hargaMakanan[2];
+            break;
+        default:
+            namaProduk = "tidak tersedia";\
+            hargaProduk = 0;
+            break;
+        }
+        break;
+    case 2:
+        cout<<"\n\t--Daftar Minuman--"<<endl;
+        for (int i = 0; i < jml_arr; i++)
+            {
+                cout<<i+1<<". "<<minuman[i]<<" : "<<hargaMinuman[i]<<endl;
+            }
+        cout<<"Pilih minuman : ";
+        cin>>(produk);
+        switch (produk)
+        {
+        case 1:
+            namaProduk = minuman[0];
+            hargaProduk = hargaMinuman[0];
+            break;
+        case 2:
+            namaProduk = minuman[1];
+            hargaProduk = hargaMinuman[1];
+            break;
+        case 3:
+            namaProduk = minuman[2];
+            hargaProduk = hargaMinuman[2];
+            break;
+        default:
+            namaProduk = " tidak tersedia";\
+            hargaProduk = 0;
+            break;
+        }
+        break;
+    default:
+        break;
+    }
+    cout<<"Ingin berapa banyak? ";
+    cin>>(jumlah);
+    cout<<"\nProduk yang anda pilih "<<namaProduk<<" sebanyak "<<jumlah<<", Total harganya adalah "<<hargaProduk*jumlah;
+}
