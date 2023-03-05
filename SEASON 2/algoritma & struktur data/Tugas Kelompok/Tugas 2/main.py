84defcdcf3712858916536a47a7944bee34f40c4
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2023-03-05 14:22:07 +0700

    update

--- SEASON 2/algoritma & struktur data/Tugas Kelompok/Tugas 2/main.py
+++ SEASON 2/algoritma & struktur data/Tugas Kelompok/Tugas 2/main.py
@@ -49,62 +49,61 @@ while True:
         print(f"{i+1}. {menu[i]}")
     
     choice = int(input(">> "))
-    match choice:
-        case 1:
-            # print("Pilihan 1")
-            if linked_list.head is None:
-                print("List masih kosong, silahkan tambahkan elemen kedalam list...")
-            else:
-                print("Linked List: ", end="")
-                linked_list.display()
-            clear_screen()
-        case 2:
-            # print("Pilihan 2")
-            data = int(input("Masukan angka untuk ditambahkan di depan list : "))
-            linked_list.insertAtBeginning(data)
-            print("Data ditambahkan di depan list...")
-            clear_screen()
-        case 3:
-            # print("Pilihan 3")
-            data = int(input("Masukan angka untuk ditambahkan di akhir list : "))
-            linked_list.insertAtEnd(data)
-            print("Data ditambahkan di akhir list...")
-            clear_screen()
-        case 4:
-            # print("Pilihan 4")
-            if linked_list.head is not None:
-                print("Linked List: ", end="")
-                linked_list.display()
-                node_data = int(input("Masukan data node mana yang ingin anda tambahkan data baru setelahnya : "))
-                node = linked_list.searchNode(node_data)    
-                if node:
-                    data = int(input(f"Masukan angka yang anda ingin tambahkan setelah {node_data} : "))
-                    linked_list.insertAfterNode(node, data)
-                    print(f"Data baru ditambahkan setelah {node_data}")
-                else: print(f"Node {node_data} tidak ditemukan di dalam linked list ini...")
-            else:
-                print("List masih kosong, silahkan tambahkan elemen kedalam list...")
-            clear_screen()
-        case 5:
-            # print("Pilihan 5")
-            data = int(input("Masukan data yang ingin dicari : "))
-            node = linked_list.searchNode(data)
-            print("Node ditemukan dalam linked list" if node else "Node tidak ditemukan dalam list")
-            clear_screen()
-        case 6:
-            # print("Pilihan 6")
-            if linked_list.head is None:
-                print("List masih kosong, silahkan tambahkan elemen kedalam list...")
-            else:
-                print("Linked List: ", end="")
-                linked_list.display()
-                data = int(input("Masukan data yang ingin dihapus : "))
-                linked_list.deleteNode(data)
-            clear_screen()
-        case 7:
-            print("Exiting...")
-            break
-        case _:
-            print("Pilihan tidak tersedia")
-            clear_screen()
+    if choice == 1:
+        # print("Pilihan 1")
+        if linked_list.head is None:
+            print("List masih kosong, silahkan tambahkan elemen kedalam list...")
+        else:
+            print("Linked List: ", end="")
+            linked_list.display()
+        clear_screen()
+    elif choice == 2:
+        # print("Pilihan 2")
+        data = int(input("Masukan angka untuk ditambahkan di depan list : "))
+        linked_list.insertAtBeginning(data)
+        print("Data ditambahkan di depan list...")
+        clear_screen()
+    elif choice == 3:
+        # print("Pilihan 3")
+        data = int(input("Masukan angka untuk ditambahkan di akhir list : "))
+        linked_list.insertAtEnd(data)
+        print("Data ditambahkan di akhir list...")
+        clear_screen()
+    elif choice == 4:
+        # print("Pilihan 4")
+        if linked_list.head is not None:
+            print("Linked List: ", end="")
+            linked_list.display()
+            node_data = int(input("Masukan data node mana yang ingin anda tambahkan data baru setelahnya : "))
+            node = linked_list.searchNode(node_data)    
+            if node:
+                data = int(input(f"Masukan angka yang anda ingin tambahkan setelah {node_data} : "))
+                linked_list.insertAfterNode(node, data)
+                print(f"Data baru ditambahkan setelah {node_data}")
+            else: print(f"Node {node_data} tidak ditemukan di dalam linked list ini...")
+        else:
+            print("List masih kosong, silahkan tambahkan elemen kedalam list...")
+        clear_screen()
+    elif choice == 5:
+        # print("Pilihan 5")
+        data = int(input("Masukan data yang ingin dicari : "))
+        node = linked_list.searchNode(data)
+        print("Node ditemukan dalam linked list" if node else "Node tidak ditemukan dalam list")
+        clear_screen()
+    elif choice == 6:
+        # print("Pilihan 6")
+        if linked_list.head is None:
+            print("List masih kosong, silahkan tambahkan elemen kedalam list...")
+        else:
+            print("Linked List: ", end="")
+            linked_list.display()
+            data = int(input("Masukan data yang ingin dihapus : "))
+            linked_list.deleteNode(data)
+        clear_screen()
+    elif choice == 7:
+        print("Keluar...")
+        break
+    else:
+        print("Pilihan tidak tersedia")
+        clear_screen()
 print ("program keluar")
